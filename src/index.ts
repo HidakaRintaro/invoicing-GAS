3d49e0c98677e7504ad9378ee6a6a1bf6da678e9
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-05-04 05:23:20 +0900

    ベースファイルの作成と大まかな処理の流れを記述

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { postSearch } from "./api"
+
+const main = () => {
+    // 請求書のIDを取得する (search)
+
+    // 請求書の宛先・住所などのブロックのIDを取得 (block/child)
+
+    // 宛先・住所などを取得
+
+
+    // 現在日時でSearchしIDを取得 (search)
+
+    // 勤怠のTableと件名などのブロックのIDを取得 (block/child)
+
+    // 件名・Noなどを取得 (block/child)
+
+    // 勤怠情報を取得 (db/query)
+
+    // 業務内容を取得 (pages) or その他の場合は別処理
+
+    // 時間の計算
+
+    // スプレッドシートに書き出し
+
+    // PDF化
+
+    // GoogleDriveに保存
+
+    // PDFのURLをNotionに書き込み
+}
